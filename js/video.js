6f6783bbdf413edb280008948fe2c6366517eb44
leomouraire1 <leomouraire1@gmail.com>
2019-06-09 01:37:53 +0200

    video remove controls and mute video

--- js/video.js
+++ js/video.js
@@ -1,16 +1,28 @@
 var video = document.getElementById("video");
 
+var soundIcon = $(".sound-icon");
 
 $("#video").ready(function() {
   TweenMax.to(video, .5, {autoAlpha: 1});
 })
 
-// video.addEventListener('click', function() {
-//
-//   if(video.muted == false) {
-//     video.muted = true
-//   } else {
-//     video.muted = false
-//   }
-//
-// });
+video.addEventListener('mousemove', function(e) {
+
+  var posX = e.clientX;
+  var posY = e.clientY;
+
+  TweenMax.to(".sound-icon", .3, {left: posX, top: posY, ease: Power1.easeInOut});
+
+})
+
+video.addEventListener('click', function() {
+
+  if(video.muted == false) {
+    // soundIcon.src="";
+    video.muted = true
+  } else {
+    // soundIcon.src="";
+    video.muted = false
+  }
+
+});
