d9daa528b6fca9e5281552fb7506691f788f11ae
leomouraire1 <leomouraire1@gmail.com>
2019-07-20 20:48:47 +0200

    add Svg shapes

--- js/modules/morph.js
+++ js/modules/morph.js
@@ -1,17 +1,31 @@
-// //MORPH SVG STUFF
-// var svg = document.getElementById("svg");
-// var img = document.getElementById("target-img");
-// console.log(img.width);
-// var s = Snap(svg);
-// var start = Snap.select("#start");
-// var end = Snap.select("#end")
-// var startPoints = start.node.getAttribute('d');
-// var endPoints = end.node.getAttribute('d');
-//
-// var toFancy = function(){
-//   start.animate({ d: startPoints }, 1000, mina.backout, toSimple);
-// }
-// var toSimple = function(){
-//   start.animate({ d: endPoints }, 1000, mina.backout, toFancy);
-// }
-// toSimple();
+// const svg = document.querySelector("wwf-svg");
+// const s = Snap(svg);
+
+// let shapes = [
+//     {
+//         className: '',
+//         d: ''
+//     },
+//     {
+//         className: '',
+//         d: ''
+//     },
+//     {
+//         className: '',
+//         d: ''
+//     },
+//     {
+//         className: '',
+//         d: ''
+//     }
+// ];
+
+// let path = Snap.select('.SVGID_1_');
+
+// console.log(path);
+
+// svg.addEventListener('mouseenter', function() {
+
+//     path.animate({ d: "M7.6,290.6C-1,312,8.8,407.4,89.5,381.5s80.4-48.2,131.9-11.6s180.4-7.9,201.1-32.4c33.9-40.1,16-61.6,84.9-79.5c31.4-8.2,65.7-89.3,35.4-122c-27.2-30.9,5.8-81.7-63.3-98.4c-58.3-14.9-109.3,15.2-129.4-3.3 c-22.9-21.8-42.2-43.1-112.7-21.3c-72,30.7-55.2,81.2-112.9,84.6c-44.3,2.9-107.1,18.7-113.7,50.1C2.1,188.9,35.4,197,7.6,290.6z" }, 1000, mina.bounce);
+
+// });
